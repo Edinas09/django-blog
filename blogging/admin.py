b3b68892722bbe6b4284cb8f7ec316af1bf5d392
edina do nascimento silva <edinas09@gmail.com>
2021-11-17 11:48:18 -0800

    add features lesson 07 assignment

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 from blogging.models import Post, Category # import all models
 
 # Register your models here.
+
+class CategoriesInline(admin.TabularInline):
+    model = Post.categories.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoriesInline, ]
+    exclude = ('categories',)
+
+
+
 # and a new admin registration
-admin.site.register(Post)
+admin.site.site_header = 'Admin Blog - Django - UW'
+# admin.site.register(Post)
+# admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
+
+
+
